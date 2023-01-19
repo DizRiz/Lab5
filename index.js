1688ec84a8fc5f2079dfa4cb39cfb51ae0b82c32
DizRiz <mdfarishakim@gmail.com>
2023-01-19 21:49:22 +0800

    Ex 4: Created controller folder and moved all operation functions here

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const express = require('express')
-const app = express()
-const port1 = 3000
-const port2 = 4000
-let calculatorRoute = require("./routes/calculatorRoute")
+const express = require("express");
+const app = express();
+const port1 = 3000;
+const port2 = 4000;
+let calculatorRoute = require("./routes/calculatorRoute");
 
-app.use('/calculator', calculatorRoute)
+app.use("/calculator", calculatorRoute);
 
-app.get('/', (req, res) => {
-res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port1, () => {
-console.log(`Web server running at localhost:${port1}`)
-})
+  console.log(`Web server running at localhost:${port1}`);
+});
 
 app.listen(port2, () => {
-    console.log(`Web server running at localhost:${port2}`)
-})
-    
-app.use('/', express.static('public'))
+  console.log(`Web server running at localhost:${port2}`);
+});
 
-app.get('/test', (req, res) => {
-    res.send('Hello World!')
-})
+app.use("/", express.static("public"));
+
+app.get("/test", (req, res) => {
+  res.send("Hello World!");
+});
--- routes/calculatorRoute.js
+++ routes/calculatorRoute.js
@@ -1,37 +1,18 @@
 const express = require("express");
+var calculatorController = require("../controllers/calculatorController");
 var router = express.Router();
 
 router.get("/add", (req, res) => {
-  let number1 = parseInt(req.query.num1);
-  let number2 = parseInt(req.query.num2);
-  let sum = number1 + number2;
-  console.log(sum);
-  res.status(200);
-  res.json({ result: sum });
+  calculatorController.addNumbers(req, res);
 });
 router.get("/sub", (req, res) => {
-    let number1 = parseInt(req.query.num1);
-    let number2 = parseInt(req.query.num2);
-    let sum = number1 - number2;
-    console.log(sum);
-    res.status(200);
-    res.json({ result: sum });
-  });
-  router.get("/multi", (req, res) => {
-    let number1 = parseInt(req.query.num1);
-    let number2 = parseInt(req.query.num2);
-    let sum = number1 * number2;
-    console.log(sum);
-    res.status(200);
-    res.json({ result: sum });
-  });
-  router.get("/div", (req, res) => {
-    let number1 = parseInt(req.query.num1);
-    let number2 = parseInt(req.query.num2);
-    let sum = number1 / number2;
-    console.log(sum);
-    res.status(200);
-    res.json({ result: sum });
-  });
-      
+  calculatorController.subNumbers(req, res);
+});
+router.get("/multi", (req, res) => {
+  calculatorController.multiNumbers(req, res);
+});
+router.get("/div", (req, res) => {
+  calculatorController.divNumbers(req, res);
+});
+
 module.exports = router;
