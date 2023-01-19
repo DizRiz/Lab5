01973df07cc477c4a4cb7f499fa030e9cb1d57ce
DizRiz <mdfarishakim@gmail.com>
2023-01-19 21:36:12 +0800

    Added public and routes folders, added 4 operations in routes

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port1 = 3000
 const port2 = 4000
+let calculatorRoute = require("./routes/calculatorRoute")
+
+app.use('/calculator', calculatorRoute)
 
 app.get('/', (req, res) => {
 res.send('Hello World!')
@@ -13,4 +16,10 @@ console.log(`Web server running at localhost:${port1}`)
 
 app.listen(port2, () => {
     console.log(`Web server running at localhost:${port2}`)
-    })
+})
+    
+app.use('/', express.static('public'))
+
+app.get('/test', (req, res) => {
+    res.send('Hello World!')
+})
